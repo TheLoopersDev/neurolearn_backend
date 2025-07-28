f1f672931f85750852e9f0b630c577863d4ddc64
theqi106 <phkhhulolmb1@gmail.com>
2025-07-28 14:33:16 +0700

    fix: fix order, request & revenue controller

--- src/controllers/revenue.controller.ts
+++ src/controllers/revenue.controller.ts
@@ -4,13 +4,13 @@ import { catchAsync } from '../utils/catchAsync';
 import Revenue from '../models/Revenue.model';
 
 export const getRevenueByUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  let userId = req.user._id.toString();
-  if (req.user.role === 'admin' && req.query.userId) {
-    userId = req.query.userId;
-  }
-  const revenue = await Revenue.find({ user: userId }).sort({ createdAt: -1 });
-  res.status(200).json({
-    success: true,
-    revenue
-  });
-}); 
+    let userId = req.user._id.toString();
+    if (req.user.role === 'admin' && req.query.userId) {
+        userId = req.query.userId;
+    }
+    const revenue = await Revenue.find({ user: userId }).sort({ createdAt: -1 });
+    res.status(200).json({
+        success: true,
+        revenue
+    });
+});
