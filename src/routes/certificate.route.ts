643dc38ee0e28c6747fb794697fd6b52af5e9ca5
Mạnh Hùng <hungoffsize@gmail.com>
2025-08-13 04:24:19 +0700

    Fix TypeScript build errors for Render deployment

--- src/routes/certificate.route.ts
+++ src/routes/certificate.route.ts
@@ -1,25 +1,20 @@
 import express from 'express';
 import { getCertificateById, getCertificateByCourse, getAllCertificates, getCertificateByUser } from '../controllers/certificate.controller';
 import { isAuthenticated } from '../middlewares/auth/isAuthenticated';
+import { catchAsync } from '../utils/catchAsync';
 
 const router = express.Router();
 
 // Lấy certificate theo ID
-router.get('/:id', isAuthenticated, (req, res, next) => {
-  getCertificateById(req, res).catch(next);
-});
+router.get('/:id', isAuthenticated, catchAsync(getCertificateById));
 
 // Lấy tất cả certificate (admin)
 router.get('/', isAuthenticated, getAllCertificates);
 
 // Lấy certificate của user cho 1 course
-router.get('/user/:userId/course/:courseId', isAuthenticated, (req, res, next) => {
-  getCertificateByUser(req, res).catch(next);
-});
+router.get('/user/:userId/course/:courseId', isAuthenticated, catchAsync(getCertificateByUser));
 
 // Lấy tất cả certificate của 1 course
-router.get('/course/:courseId', isAuthenticated, (req, res, next) => {
-  getCertificateByCourse(req, res).catch(next);
-});
+router.get('/course/:courseId', isAuthenticated, catchAsync(getCertificateByCourse));
 
 export default router;
--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,10 +1,21 @@
-import { UserT } from '../interfaces/User';
+import { Request } from 'express';
 
 declare global {
     namespace Express {
         interface Request {
-            user?: UserT;
             access_token?: string;
+            user?: any;
+            file?: Express.Multer.File;
+            files?: { [fieldname: string]: Express.Multer.File[] };
         }
+    }
+}
+
+declare module 'express' {
+    interface Request {
+        access_token?: string;
+        user?: any;
+        file?: Express.Multer.File;
+        files?: { [fieldname: string]: Express.Multer.File[] };
     }
 }
