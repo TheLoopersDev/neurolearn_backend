338da18ab98c656491428926085ab42418be2316
theqi106 <phkhhulolmb1@gmail.com>
2025-08-14 15:23:14 +0700

    fix: fix build

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -2,9 +2,25 @@ import { UserT } from '../interfaces/User';
 
 declare global {
     namespace Express {
+        namespace Multer {
+            interface File {
+                fieldname: string;
+                originalname: string;
+                encoding: string;
+                mimetype: string;
+                size: number;
+                destination?: string;
+                filename?: string;
+                path?: string;
+                buffer: Buffer;
+                stream?: import('stream').Readable;
+            }
+        }
         interface Request {
             user?: UserT;
             access_token?: string;
+            file?: Multer.File;
+            files?: Multer.File[] | { [fieldname: string]: Multer.File[] };
         }
     }
 }
