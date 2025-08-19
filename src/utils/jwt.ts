c028a3f7fdd279739324a369cf055b0c8b1a1c3d
anhlnp <anhphucpro13@gmail.com>
2025-08-20 02:31:52 +0700

    fix: auth

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -21,7 +21,7 @@ export const accessTokenOptions: ITokenOptions = {
     expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
     maxAge: accessTokenExpire * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
+    secure: isProd,
     path: '/',
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
     // secure: process.env.NODE_ENV === 'production',
@@ -31,7 +31,7 @@ export const refreshTokenOptions: ITokenOptions = {
     expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
     maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
+    secure: isProd,
     path: '/',
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
 };
